0c7bf7b40a88e1b965373909e4e95cc8d3fa27bc
Ansuz <adrianmrv02@gmail.com>
2024-04-02 14:58:18 +0200

    added core readonly dictionary

---
+++ Amrv.ConfigurableCompany/Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amrv.ConfigurableCompany.Core.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static ReadonlyDictionary<K, V> AsReadOnly<K, V>(this Dictionary<K, V> dictionary) => new(dictionary);
+    }
+}
---
+++ Amrv.ConfigurableCompany/Core/ReadonlyDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amrv.ConfigurableCompany.Core
+{
+    public sealed class ReadonlyDictionary<K, V>(Dictionary<K, V> wrapper) : IReadOnlyDictionary<K, V>
+    {
+        public V this[K key] => wrapper.GetValueOrDefault(key, default);
+
+        public IEnumerable<K> Keys => wrapper.Keys;
+
+        public IEnumerable<V> Values => wrapper.Values;
+
+        public int Count => wrapper.Count;
+
+        public bool ContainsKey(K key) => wrapper.ContainsKey(key);
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator() => wrapper.GetEnumerator();
+
+        public bool TryGetValue(K key, out V value) => wrapper.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() => wrapper.GetEnumerator();
+    }
+}
