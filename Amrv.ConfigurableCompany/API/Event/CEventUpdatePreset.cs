6f834ee6bc1613bbe6a7ee7cb910f400776f4024
Ansuz <adrianmrv02@gmail.com>
2024-04-05 10:38:58 +0200

    Added missing event values

--- Amrv.ConfigurableCompany/API/Event/CEventUpdatePreset.cs
+++ Amrv.ConfigurableCompany/API/Event/CEventUpdatePreset.cs
@@ -1,7 +1,4 @@
 ﻿using Amrv.ConfigurableCompany.Core.Config;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using static Amrv.ConfigurableCompany.API.Event.CEventUpdatePreset;
 
 namespace Amrv.ConfigurableCompany.API.Event
@@ -9,6 +6,8 @@ namespace Amrv.ConfigurableCompany.API.Event
     public class CEventUpdatePreset(string file, PresetAction action) : CEvent
     {
         public string Folder => Presets.Folder;
+        public readonly string File = file;
+        public readonly PresetAction Action = action;
         public enum PresetAction
         {
             CREATE,
