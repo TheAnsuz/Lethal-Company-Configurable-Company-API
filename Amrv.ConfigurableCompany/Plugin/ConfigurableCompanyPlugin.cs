86d6f6a0ca918bbb09af790e8305860bb31fd6e0
Ansuz <adrianmrv02@gmail.com>
2024-08-06 13:34:02 +0200

    Updated project version

--- Amrv.ConfigurableCompany/Plugin/ConfigurableCompanyPlugin.cs
+++ Amrv.ConfigurableCompany/Plugin/ConfigurableCompanyPlugin.cs
@@ -21,7 +21,7 @@ namespace Amrv.ConfigurableCompany.Plugin
     {
         public const string PLUGIN_GUID = "dev.amrv.lethalCompany.config";
         public const string PLUGIN_NAME = "Configurable Company";
-        public const string PLUGIN_VERSION = "3.4.0";
+        public const string PLUGIN_VERSION = "3.5.0";
 
         /// <summary>
         /// Plugin folder
